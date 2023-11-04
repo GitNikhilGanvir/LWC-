a7fca29102eb28117dfe2a0a02eb71339224f44e
Nikhil Ganvir <117508736+GitNikhilGanvir@users.noreply.github.com>
2023-11-04 18:17:05 +0530

    Delete dataBindingTwoWay.js

--- dataBindingTwoWay.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class DataBindingTwoWay extends LightningElement {
-    inputValue='Type Here';
-    handleChange(event)
-    {
-        this.inputValue=event.target.value;
-    }
-}
